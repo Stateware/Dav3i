3f9a7cabf66f8d646d085927e2f460a9cee4dbcd
Nicholas Denaro <NicholasCDenaro@aol.com>
2015-02-10 19:40:42 -0500

    Just for shits

---
+++ GUI/GUI/parser.js
@@ -0,0 +1,34 @@
+﻿function Parser(input)
+{
+    this.input = input;
+    this.getParsedData = parseData;
+}
+
+
+function parseData(json, settings)
+{
+    //Check to see what type of scheme we are outputting using settings
+
+    //reading the json!!!
+
+    //returning some sort of array
+
+    return (json.data[1]);
+}
+
+function runThis()
+{
+    var json;
+    json = {
+        settings: "settings",
+        data: [
+            "data0",
+            "data1",
+            "data2"]
+    };
+    var t_parser = new Parser(json);
+
+    var retval = t_parser.getParsedData(json, json.setings);
+
+    alert(retval);
+}
