5cb8f89ebff60e2c0aafb4e50d6fbc8a2333ae37
Andrew Lopreiato <dLopreiato@users.noreply.github.com>
2015-02-13 13:53:25 -0500

    Remove personal database details from Connect.php

--- src/Connect.php
+++ src/Connect.php
@@ -1,31 +1,29 @@
 <?php
 /* File Name:           Connect.php
- * Description:         This file connects to the database using
- *                      the proper hostname/username/password/dbname
+ * Description:         This file contains a function to create a mysqli connection
  * 
  * Date Created:        2/7/2015
  * Contributors:        Kyle Nicholson, Berty Ruan
- * Date Last Modified:  2/8/2015
- * Last Modified By:    Kyle Nicholson, Berty Ruan
- * Dependencies:        NULL
+ * Date Last Modified:  2/13/2015
+ * Last Modified By:    Drew Lopreiato
+ * Dependencies:        Toolbox.php
  * Input:               NONE
- * Output:              $conn -> this is a connection to the database
- * Additional Notes:    when we get the real database we will input the 
- *                      correct hostname/username/password/dbname
+ * Output:              NONE
+ * Additional Notes:    Replace HOST, USER, PASSWORD, and DATABASE with information relvant to your build.
  */
 require_once("Toolbox.php");
 
 
 function GetDatabaseConnection()
 {
-	$databaseConnection = new mysqli("localhost", "root", "", "peachtree");
+    $databaseConnection = new mysqli(HOST, USER, PASSWORD, DATABASE);
 
-	if ($databaseConnection->connect_error)
-	{
-		ThrowFatalError("Could not connect to database.");
-	}
+    if ($databaseConnection->connect_error)
+    {
+        ThrowFatalError("Could not connect to database.");
+    }
 
-	return $databaseConnection;
+    return $databaseConnection;
 } // END GetDatabaseConnection
 
 ?>
