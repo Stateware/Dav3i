0ff7afbef9e0746e7f8a535d6dc804b62ce6d87b
CarpeDiEmma <ejroud@gmail.com>
2015-02-24 15:43:59 -0500

    Changes to Loading Screen
    Clean-up files, fixing javascript

--- src/GUI/loadingScreen.js
+++ src/GUI/loadingScreen.js
@@ -1,4 +1,28 @@
+﻿// File Name:               loadingScreen.js
+// Description:             Actions of the loading screen, including creating the initial lookup table and heat map
+// Date Created:            2/19/2015
+// Contributors:            Emma Roudabush
+// Date Last Modified:      2/23/2015
+// Last Modified By:        Emma Roudabush
+// Dependencies:            loadingScreen.html, loadingScreen.css
+// Additional Notes:        N/A
+
 $(document).ready(function () {
+	// Generate heat map
+	var jqxhr = $.ajax({
+		type: "get",
+		url: "http://usve74985.serverprofi24.com/API/descriptor.php",
+		dataType: "json",
+		success:function(data) {
+			alert("success");
+			console.log(JSON.Stringify(data));
+		},
+		error:function(error) {
+			alert("error");
+			console.log(error);
+		}
+	});
+	// Do below process when heat map is generated
     setTimeout(function () {
         $(".spinner").fadeOut(1250);
     }, 3000);
@@ -7,10 +31,6 @@ $(document).ready(function () {
     }, 4250);
 });
 
-$(".begin").click(function () {
-     $.getJSON("http://usve74985.serverprofi24.com/fake_descrip", function(result){
-        $.each(result, function(i, field){
-            $("div").append(field + " ");
-        });
-    });
+$(".begin").click(function(){
+    // Go to main page
 });
