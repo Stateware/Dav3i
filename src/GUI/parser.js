c5f5db91af7bf1f944683ff98e0161b9f75e5b49
Nicholas Denaro <NicholasCDenaro@aol.com>
2015-02-24 19:18:57 -0500

    Modified parser to properly handle countries in the order that they were queried.

--- src/GUI/parser.js
+++ src/GUI/parser.js
@@ -1,11 +1,11 @@
 ﻿// File Name:               parser.js
-// Description:             Parses JSON, in the format of {country,stat,data}, into a 3D array, to be
+// Description:             Parses JSON, in the format of {stat,country/bound,data}, into a 3D array, to be
 //                          used by the graphs
 // Date Created:            2/12/2015
 // Contributors:            Nicholas Denaro
 // Date Last Modified:      2/19/2015
 // Last Modified By:        Nicholas Denaro
-// Dependencies:            parser_markup.js(loosely)
+// Dependencies:            None
 // Additional Notes:        N/A
 
 // Author:          Nicholas Denaro
@@ -14,23 +14,23 @@
 // Description:     Parses the object that is passed in and returns data array.
 //                  Input: json - Assumed to be in the proper format, but checked later in function, and sets a flag
 //                                if the JSON is invalid
-//                  Output: data - a 3D array in the format of [stat][country/bound][values];
-function ParseData(json)
+//                         cids - An array of country IDs in the same order that were sent to the server.
+//                  Output: data - A 3D array in the format of [stat][country/bound][values];
+function ParseData(json,cids)
 {
     var data = new Array(); //Creates the array for the data to be returned
     var valid = true; //Set flag to true, and chagne to false if the JSON is not valid.
 
     //Loops through the countries in the JSON
-    for (var country = 1; json.hasOwnProperty(country) ; country++) //Uses hasOwnProperty because properties aren't in
-                                                                    //a form to check for a length.
+    for (var cid=0; cid<cids.length ; cid++)
     {
         //Loops through the stats in the JSON
-        for (var stat = 1; json[country].hasOwnProperty(stat) ; stat++)
+        for (var stat = 1; json[cids[cid]].hasOwnProperty(stat) ; stat++)
         {
             if (data[stat - 1] == null) //Checks if the array has been initilaized, if not, create new Array at index
                 data[stat - 1] = new Array();
-            data[stat - 1][country - 1] = json[country][stat]; //Sets the data for the specified stat,country/bound to 
-                                                               //the array in the json.
+            data[stat - 1][cid] = json[cids[cid]][stat]; //Sets the data for the specified stat,country/bound to 
+                                                         //the array in the json.
         }
     }
 
@@ -63,7 +63,7 @@ function ParseFakeData()
 	        2: [3, 4, 4],//array2
 	        3: [5, 6, 7]
 	    },
-        "2"://country 2
+        "34"://country 2
 	    {
 	        1: [5, 6, 7],
 	        2: [5, 7, 9],
@@ -75,7 +75,7 @@ function ParseFakeData()
 
     var data = JSON.parse(document.getElementById("jsonInput").value);
 
-    var retval = ParseData(data);
+    var retval = ParseData(data, [86, 34, 57]);
 
     document.getElementById("dataOutput").value = JSON.stringify(retval, "\n", 4);
 }
