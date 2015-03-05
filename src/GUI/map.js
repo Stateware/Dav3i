c30bbd50530f38b0b21e53c2e85cbbcec5a9832b
vanajamsoni <Vanajam@Vanajams-MacBook-Pro.local>
2015-03-05 18:48:40 -0500

    updates after code review

--- src/GUI/map.js
+++ src/GUI/map.js
@@ -1,18 +1,19 @@
-// File Name:               map.js
-// Description:             Initialization of map
-// Date Created:            2/24/2015
-// Contributors:            Vanajam Soni
-// Date Last Modified:      2/26/2015
-// Last Modified By:        Vanajam Soni
-// Dependencies:            index.html
-// Additional Notes:        N/A
+// File Name:				map.js
+// Description:				This module generates a map inside the div 'map', and
+//							defines a listener for the button "clear"
+// Date Created:			2/24/2015
+// Contributors:			Vanajam Soni
+// Date Last Modified:		3/5/2015
+// Last Modified By:		Vanajam Soni
+// Dependencies:			N/A
+// Additional Notes:		N/A
 
 
 // Author: Vanajam Soni
-// Date Created: 3/4/2015
-// Last Modified: 3/4/2015 by Vanajam Soni
+// Date Created: 2/24/2015
+// Last Modified: 3/5/2015 by Vanajam Soni
 // Description: This function initializes the map, fills up the textarea 'cc2_
-// 				selected' with the list of selected regions and clears the map
+//				selected' with the list of selected regions and clears the map
 //				on the click of button "clear"
 // PRE: index.html and div with id "map" exist 
 // POST: The map is initialized in the div "map"
@@ -20,22 +21,22 @@ $(function()
 {
 	var map = new jvm.Map(
 	{
-		container: $('#map'),
-		regionsSelectable: true,
+		container: $('#map'),	// container div for the map
+		regionsSelectable: true,	// allows us to select regions
 		
-		// colors for selected and non-selected regions
 		regionStyle: 
 		{
-			initial: 
+			initial:	// color of non-selected regions
 			{
 				fill: '#FFFFFF'
 			},
-			selected: 
+			selected:	// color of selected regions
 			{
 				fill: '#F4A582'
 			}
 		},
 		
+		// script to run when a region is selected
 		onRegionSelected: function()
 		{
 			// Storing selected regions in the windows local cache
@@ -49,21 +50,10 @@ $(function()
 			
 			// Filling the textarea with list of regions selected 
 			document.getElementById('cc2_selected').value = JSON.stringify(map.getSelectedRegions());
-		},
-		onMarkerSelected: function()
-		{
-			if (window.localStorage) 
-			{
-				window.localStorage.setItem(
-					'jvectormap-selected-markers',
-					JSON.stringify(map.getSelectedMarkers())
-				);
-			}
 		}
 	});
 	
-	// in case of a refresh, setting selected regions to the list of regions in
-	// cache
+	// in case of a refresh, setting selected regions to the list of regions in cache
 	map.setSelectedRegions( JSON.parse( window.localStorage.getItem('jvectormap-selected-regions') || '[]' ) );
 	map.setSelectedMarkers( JSON.parse( window.localStorage.getItem('jvectormap-selected-markers') || '[]' ) );
 	
