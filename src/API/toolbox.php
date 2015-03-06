8c87a6b6b3bb83326aae8516f869f865fa18ca36
Drew Lopreiato <drew.lopreiato@gmail.com>
2015-03-05 19:12:37 -0500

    Document the effects of testing define

--- src/API/toolbox.php
+++ src/API/toolbox.php
@@ -1,7 +1,7 @@
 <?php
 /* File Name:           toolbox.php
  * Description:         This will be included in nearly every PHP file we write and it will hold commonly
- * 							used functions and any globals we may need.
+ *                             used functions and any globals we may need.
  * Date Created:        2/12/2015
  * Contributors:        Drew Lopreiato, William Bittner, Kyle Nicholson, Berty Ruan, Arun Kumar
  * Date Last Modified:  2/20/2015
@@ -10,40 +10,37 @@
  * Input:               none
  * Output:              none
  */ 
-
-//change to TRUE if testing so the ThrowFatalError doesn't kill the page
-define("TESTING", TRUE);
-	 
-// ===================== Function Definitions =====================	 
+     
+// ===================== Function Definitions =====================     
 //OUTPUT: This function will cause the program to close itself after echoing an error message.
 //INPUT: string message - optional further description of error
 function ThrowFatalError($message = "An Error has occured.") 
 {
     echo "{\"error\" : \"" . $message . "\"}";
     if(!TESTING)
-	{
-		die();
-	}
+    {
+        die();
+    }
 } // END ThrowFatalError
 
 
 //OUTPUT: The first row from the "columnName" column of the "tableName" table of the "database" database
-//			with optional "filter" filter
-//INPUTS: 	database: the database to query
-//			tableName: the table in the database to get the data from
-//			columnName: the column of the table from which to return the first row
-//			filter: when supplied, will add a filter to the query
+//            with optional "filter" filter
+//INPUTS:     database: the database to query
+//            tableName: the table in the database to get the data from
+//            columnName: the column of the table from which to return the first row
+//            filter: when supplied, will add a filter to the query
 function GetFirstRowFromColumn($database, $tableName, $columnName, $filter = false)
 {
-	$query = "SELECT " . $columnName . " FROM " . $tableName;
-	if ($filter !== false)
-	{
-		$query .= " WHERE " . $filter;
-	}
-	$results = $database->query($query);
-	$row = $results->fetch_assoc();
-	$results->free();
-	return $row[$columnName];
+    $query = "SELECT " . $columnName . " FROM " . $tableName;
+    if ($filter !== false)
+    {
+        $query .= " WHERE " . $filter;
+    }
+    $results = $database->query($query);
+    $row = $results->fetch_assoc();
+    $results->free();
+    return $row[$columnName];
 } // END GetFirstRowFromColumn
 
 
@@ -51,5 +48,9 @@ function GetFirstRowFromColumn($database, $tableName, $columnName, $filter = fal
 // These are global variables that describe our default values for data of the given types
 define("DEFAULT_NUMBER", -1);
 define("DEFAULT_STRING", "");
-//throw descriptor in here? we will need it to validate the entry fields in both data returning fn's
+/*      EFFECTS OF TESTING == TRUE:
+ * 1. the ThrowFatalError doesn't kill the page
+ * 2. the API calls can be made from 
+ */
+define("TESTING", TRUE);
 ?>
