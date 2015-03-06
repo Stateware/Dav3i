ec149f9cb917acf4168eeddc47e5ceb8bbbb35f4
Berty Ruan <berty.ruan@gmail.com>
2015-03-05 19:54:29 -0500

    update on testing script & fixed bugs

--- src/API/test_lib.php
+++ src/API/test_lib.php
@@ -17,71 +17,94 @@ require_once("api_library.php");
 
 $databaseConnection = GetDatabaseConnection();
 
+
+//---------------------Testing: GetYearRange()-----------------------
+
+echo "<h1> Testing for GetYearRange</h1>";
+echo "<h4> GetYearRange(databaseConnection, data_birts) == array(1980, 2012)</h4>"; 
+
 if (GetYearRange($databaseConnection, "data_births") == array("1980", "2012")) 
 {
-	echo "Test 1 for GetYearRange Passed. This is good. <br>";
+	echo "Test 1 for GetYearRange Passed. <br>";
+	//echo GetYearRange($databaseConnection, "data_births");
 }
 else
 {
-	echo "Test 1 for GetYearRange failed. This is bad. <br>";
+	echo "Test 1 for GetYearRange failed. <br>";
 }
+
+echo "<h4> GetYearRange(databaseConnection, data_cases) == array(1980, 2012))</h4>";
 
 if (GetYearRange($databaseConnection, "data_cases") == array("1980", "2012")) 
 {
-	echo "Test 2 for GetYearRange Passed. This is good. <br>";
+	echo "Test 2 for GetYearRange Passed. <br>";
+	//echo GetYearRange($databaseConnection, "data_cases");
 }
 else
 {
-	echo "Test 2 for GetYearRange failed. This is bad. <br>";
+	echo "Test 2 for GetYearRange failed. <br>";
 }
 
-//The following should not pass.
-/*****************************************************************************
+
+echo "<h4>GetYearRange(databaseConnection, data_births) == array(1900, 2012))</h4>";
+
 if (GetYearRange($databaseConnection, "data_births") == array("1900", "2012")) 
 {
-	echo "Test 3 for GetYearRange failed. This is good. <br>";
+	echo "Test 3 for GetYearRange passed. <br>";
 }	
 else
 {
-	echo "Test 3 for GetYearRange passed. This is bad. <br>";
-}
-if (GetYearRange($databaseConnection, "data_births") == array("1980", "2000")) 
-{
-	echo "Test 4 for GetYearRange failed. This is good. <br>";
-}
-else
-{
-	echo "Test 4 for GetYearRange passed. This is bad. <br>";
-}
-*****************************************************************************/
-
-if (GetStatNames($databaseConnection) == array("Births", "Cases"))
-{
-	echo "Test 1 for GetStatNames Passed. This is good. <br>";
-}
-else
-{
-	echo "Test 1 for GetStatNames failed. This is bad. <br>";
+	echo "Test 3 for GetYearRange failed. <br>";
 }
 
-//The following should not pass.
-/*****************************************************************************
-if (GetStatNames($databaseConnection)!= array("Births", "Cases"))
+echo "<h4>GetYearRange(databaseConnection, data_births) == array(1980, 2000)</h4>";
+
+if (GetYearRange($databaseConnection, "data_births") == array("1980", "2012")) 
 {
-	echo "Test 2 for GetStatNames failed. This is good. <br>";
+	echo "Test 4 for GetYearRange passed. <br>";
 }
 else
 {
-	echo "Test 2 for GetStatNames passed. This is bad. <br>";
+	echo "Test 4 for GetYearRange failed. <br>";
 }
-if (GetStatNames($databaseConnection)== array("Cases", "Births"))
+
+
+
+
+echo "<h1> Testing for GetStatNames(conn) </h1>";
+
+echo "<h4> GetStatNames(databaseConnection) == array(Births, Cases, Deaths) </h4>";
+
+if (GetStatNames($databaseConnection) == array("Births", "Cases", "Deaths"))
 {
-	echo "Test 3 for GetStatNames failed. This is good. <br>";
+	echo "Test 1 for GetStatNames passed. <br>";
 }
 else
 {
-	echo "Test 3 for GetStatNames passed. This is bad. <br>";
+	echo "Test 1 for GetStatNames failed. <br>";
 }
-*****************************************************************************/
+
+echo "<h4> GetStatNames(databaseConnection) != array(Births, Cases, Deaths) </h4>";
+
+if (GetStatNames($databaseConnection) != array("Births", "Cases", "Deaths"))
+{
+	echo "Test 2 for GetStatNames passed. <br>";
+}
+else
+{
+	echo "Test 2 for GetStatNames failed. <br>";
+}
+
+echo "<h4> GetStatNames(databaseConnection) == array(Births, Cases, Deaths) </h4>";
+
+if (GetStatNames($databaseConnection)== array("Cases", "Births", "Deaths"))
+{
+	echo "Test 3 for GetStatNames passed. <br>";
+}
+else
+{
+	echo "Test 3 for GetStatNames failed. <br>";
+}
+
 
 ?>
