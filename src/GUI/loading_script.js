c2a71d5c988aad1263918e8d5b461b5cd121f37b
jcrafts318 <jcrafts318@gmail.com>
2015-03-17 18:59:54 -0400

    Removed Wait on Load
    Removed wait and fixed name of switch to main function, also added
    animation for removal of loading screen

--- src/GUI/loading_script.js
+++ src/GUI/loading_script.js
@@ -2,27 +2,26 @@
 // Description:             For loading screen
 // Date Created:            3/5/2015
 // Contributors:            Emma Roudabush, Joshua Crafts
-// Date Last Modified:      3/15/2015
+// Date Last Modified:      3/17/2015
 // Last Modified By:        Joshua Crafts
 // Dependencies:            lookup_table.js, map.js, index.html
 // Additional Notes:        N/A
 
 //
 $(".loadingScreen").ready(function () {
-    // Generate lookup table and heat map
+// Generate lookup table and heat map
     CreateLookupTable();
-    // Do below process when heat map is generated
-    setTimeout(function () {
+// Do below process when heat map is generated
     $(".spinner").fadeOut(1250);
-    }, 3000);
     setTimeout(function () {
         $(".begin").fadeIn(1500);
-    }, 4250);
+    }, 1250);
 });
 
+
 //
-function switchToMain ()
+function SwitchToMain ()
 {
-    $(".loading-screen").hide();
+    $(".loading-screen").fadeOut(750);
 }
 
