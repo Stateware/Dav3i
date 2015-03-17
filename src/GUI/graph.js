3b39376cf350fd3d29a070cd50927b29f4b3252b
Nick Dyszel <9a5327fe@opayq.com>
2015-03-17 19:27:28 -0400

    adding signatures of graphing functions

---
+++ src/GUI/graph.js
@@ -0,0 +1,62 @@
+﻿// File Name: graph.js
+// Description: This file generates graphs based on parsed data
+// Date Created: 3/17/2015
+// Contributors: Nicholas Dyszel
+// Date Last Modified: 3/17/2015
+// Last Modified By: Nicholas Dyszel
+// Dependencies: [[ graphing API ]]
+// Additional Notes: N/A
+
+// Author: Nicholas Dyszel
+// Date Created: 3/17/2015
+// Last Modified: 3/17/2015 by Nicholas Dyszel
+// Description: Takes 1 stat ID and stat data and generates a graph
+// PRE:
+// POST:
+function GenerateSingle(statID, data) {
+
+}
+
+// Author:
+// Date Created:
+// Last Modified:
+// Description: Takes 1 stat ID and stat data and generates a graph
+//     using the
+// PRE: statID indicates a valid and bounded stat
+// POST:
+function GenerateSingleBounded(statID, data) {
+
+}
+
+// Author:
+// Date Created:
+// Last Modified:
+// Description: Takes 1 stat ID and stat list and generates a graph
+//     using data from multiple countries
+// PRE:
+// POST:
+function GraphMultiple(statID, list) {
+
+}
+
+// Author:
+// Date Created:
+// Last Modified:
+// Description: Takes 1 stat ID and stat list and generates a graph
+//     of the sum of the countries' data
+// PRE:
+// POST:
+function GenerateSum(statID, list) {
+
+}
+
+// Author:
+// Date Created:
+// Last Modified:
+// Description: Takes 1 stat ID and stat list and generates a graph
+//     of the sum of the countries' data as well as their bounds
+// PRE: statID indicates a valid and bounded stat
+// POST:
+function GenerateSumBounded(statID, list) {
+
+}
