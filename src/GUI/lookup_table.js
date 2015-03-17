9b5950c6699c6570db13b26684b2a9a1a3bb27d1
CarpeDiEmma <ejroud@gmail.com>
2015-03-17 19:58:43 -0400

    Changes to Descriptor
    Note that Descriptor doesn't work... outside of Ajax g_DescriptorJSON
    doesn't reference

--- src/GUI/lookup_table.js
+++ src/GUI/lookup_table.js
@@ -8,27 +8,37 @@
 // Additional Notes:        N/A
 
 var g_DescriptorJSON;
+var g_LookupTable;
+var g_Stats;
+
+function LookupTable(cc2, countryName, hms) {
+    this.CC2 = cc2;
+    this.CountryName = countryName;
+    this.HMS = hms;
+}
 
 function CreateLookupTable ()
 {
     GetDescriptor();
-    GetHMS();
+	console.log(g_DescriptorJSON);
     GenerateLookupTable();
+	console.log(g_LookupTable);
     GenerateStatReferenceList();
+	console.log(g_Stats); 
 }
 
 function GetDescriptor ()
 {
 	$.ajax({                                      
-		type: 'post',
 		url: 'http://usve74985.serverprofi24.com/API/descriptor.php',                                                     
-		dataType: 'JSON',                     
+		dataType: 'JSON',                 
+		async: 'false',
 		success: function(data){     
 			console.log("Successfully received descriptor.php");
 			console.log(JSON.stringify(data));
-			g_DescriptorJSON = data; 
+			g_DescriptorJSON = JSON.stringify(data);
 		} 
-	});  
+	});
 }
 
 function GetHMS ()
@@ -36,12 +46,32 @@ function GetHMS ()
 	
 }
 
+function SetHMS ()
+{
+}
+
+// Generate with HMS column as 0
 function GenerateLookupTable ()
 {
+	var CC2 = [];
+	var CountryName = [];
+	var HMS = []; 
+	for (i = 0; i < 194; i++)
+	{
+		CC2[i] = g_DescriptorJSON.cc2[i];
+		CountryName[i] = g_DescriptorJSON.common_name[i];
+		HMS[i] = 0;
+	}
 	
+	g_LookupTable = new LookupTable(CC2, CountryName, HMS);
 }
 
 function GenerateStatReferenceList()
 {
-	
+	g_Stats = []; 
+	for (i = 0; i < 194; i++)
+	{
+		g_Stats[i] = g_DescriptorJSON.stats[i];
+	}
 }
+
