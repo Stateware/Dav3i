ab9976e2291040ffd1f7c846500d412743ec6f9e
vanajamsoni <Vanajam@client-75-102-76-209.mobility-up.psu.edu>
2015-03-19 19:21:56 -0400

    data_query edits

--- src/GUI/data_query.js
+++ src/GUI/data_query.js
@@ -8,35 +8,6 @@
 // Dependencies:            lookup_table.js, byCountry.php, parser.js
 // Additional Notes:        N/A
 
-// Author:          Paul Jang
-// Date Created:    3/5/15
-// Last Modified:   3/5/15 by Paul Jang
-// Description:     Feeds CC2 codes into the lookup table and receives CIDs
-//                  Input: CC2 codes
-//                  Output: CID
-function GetCID(cc2)
-{
-}
-
-// Author:          Paul Jang
-// Date Created:    3/5/15
-// Last Modified:   3/5/15 by Paul Jang
-// Description:     queries the database for the JSON using the CIDs
-//                  Input: CIDs, from the getCID function
-//                  Output: the JSON from the database
-function QueryJSON(cids)
-{
-}
-
-// Author:          Paul Jang
-// Date Created:    3/5/15
-// Last Modified:   3/5/15 by Paul Jang
-// Description:     Takes a JSON and parses it into an array
-//                  Input: JSON, queried from the database
-//                  Output: the parsed array of data
-function ParseJSON(json)
-{
-}
 
 // Author:          Paul Jang
 // Date Created:    3/5/15
@@ -46,11 +17,55 @@ function ParseJSON(json)
 //                  Output: array of data for the map
 function GetData(cc2)
 {
-	GetCID(// cc2s from ?);
-	QueryJSON(// cids from GetCID);
-	var outputArray = ParseJSON();
-	return outputArray;
+	var cid = GetCID(cc2);
+	var dataJSON ;
+	// the code to get data from server (block below) is going to change
+	
+	$.ajax({                                      
+		url: 'http://usve74985.serverprofi24.com/API/by_country.php?CID='.concat(cid.toString()),                                                     
+		dataType: 'JSON',                 
+		async: false,
+		success: function(data){     
+			console.log("Successfully received descriptor.php");
+			dataJSON = data;
+		} 
+	});
+	  
+	
+	var parsedData = ParseJSON(dataJSON);
+	
+	var newNode = new t_AsdsNode(cid,g_LookupTable[cid][1],parsedData);
+	
+	
+	return newNode;
 }
 
 
+function AddToData(var newNode) {
+	
+}
 
+
+function RemoveFromData(var cid) {
+
+}
+
+function GetHMS(hmsId)
+{
+	var hmsData;
+	// 1. Query by_stat.php
+	
+	$.ajax({                                      
+		url: 'http://usve74985.serverprofi24.com/API/by_stat.php?statID='.concat(hmsID.toString()),                                                     
+		dataType: 'JSON',                 
+		async: false,
+		success: function(data){     
+			console.log("Successfully received descriptor.php");
+			hmsData = data;
+		} 
+	});
+	
+	
+	
+	return hmsData;
+}
