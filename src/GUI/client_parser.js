2d9bd02f2557dae0c7a3f5b644e420ce6f3adfd5
Nicholas Denaro <NicholasCDenaro@aol.com>
2015-03-19 19:32:21 -0400

    Modified the client parser to return a 2D array, corresponding to the change in the architecture document.

--- src/GUI/client_parser.js
+++ src/GUI/client_parser.js
@@ -3,37 +3,26 @@
 //                          used by the graphs
 // Date Created:            2/12/2015
 // Contributors:            Nicholas Denaro, Vanajam Soni, Paul Jang
-// Date Last Modified:      3/4/2015
+// Date Last Modified:      3/19/2015
 // Last Modified By:        Nicholas Denaro
 // Dependencies:            None
 // Additional Notes:        N/A
 
 // Author:          Nicholas Denaro
 // Date Created:    2/12/15
-// Last Modified:   3/5/15 by Nicholas Denaro
+// Last Modified:   3/19/15 by Nicholas Denaro
 // Description:     Parses the object that is passed in and returns data array.
-//                  Input: json - Assumed to be in the proper format, but checked later in function, and sets a flag
+//                  Input: json - Assumed to be in the proper format
 //                                if the JSON is invalid
-//                         cids - An array of country IDs in the same order that were sent to the server.
-//                  Output: data - A 3D array in the format of [stat][country/bound][values];
-function ParseData(json, cids)
-// PRE: json is valid JSON and cids is an array with the same size as the countries in json
-// POST: FCTVAL == a 3d array containing stat, country/bound, data
+//                  Output: data - A 2D array in the form [stat][year]
+function ParseData(json)
+// PRE: json is valid JSON with data for only one country
+// POST: FCTVAL == a 2d array containing stat, year
 {
-    var data = new Array(); //Creates the array for the data to be returned
-
-    //Loops through the countries in the JSON
-    for (var cid=0; cid<cids.length ; cid++)
-    {
-        //Loops through the stats in the JSON
-        for (var stat = 1; json[cids[cid]].hasOwnProperty(stat) ; stat++)
-        {
-            if (data[stat - 1] == null) //Checks if the array has been initilaized, if not, create new Array at index
-                data[stat - 1] = new Array();
-            data[stat - 1][cid] = json[cids[cid]][stat]; //Sets the data for the specified stat,country/bound to 
-                                                         //the array in the json.
-        }
-    }
+    var data = new Array(); // Creates the array for the data to be returned
+    data = json[Object.keys(json)[0]];// Since there will only be one country in each json,
+                                      // we can simply get the first key, and use that to
+                                      // get the value for the data.
 
     return (data);
 }
@@ -41,8 +30,8 @@ function ParseData(json, cids)
 
 // Author:          Nicholas Denaro
 // Date Created:    2/12/15
-// Last Modified:   2/24/15 by Nicholas Denaro
-// Description:     Passes fake data from the textfields, jsonInput and arrayInput, into the parser and outputs the
+// Last Modified:   3/19/15 by Nicholas Denaro
+// Description:     Passes fake data from the textfield, jsonInput, into the parser and outputs the
 //                  result into the textfield, dataOutput.
 //                  Input: N/A
 //                  Output: N/A
@@ -50,29 +39,9 @@ function ParseFakeData()
 // PRE: This function is called from parser_markup.html
 // POST: FCTVAL == null
 {
-    //This is how we think the JSON looks.
-    /*var data =
-    {
-        "1"://country 1
-	    {
-	        1: [1, 2, 3],//array1
-	        2: [3, 4, 4],//array2
-	        3: [5, 6, 7]
-	    },
-        "34"://country 2
-	    {
-	        1: [5, 6, 7],
-	        2: [5, 7, 9],
-	        3: [5, 8, 11]
-	    }
-    };*/
-
-    //{"1":{"1":[1,2,3]}}
-
     var data = JSON.parse(document.getElementById("jsonInput").value);
-    var array = JSON.parse(document.getElementById("arrayInput").value);
 
-    var retval = ParseData(data, array);
+    var retval = ParseData(data);
 
     document.getElementById("dataOutput").value = JSON.stringify(retval, "\n", 4);
 }
