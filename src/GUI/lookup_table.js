bae8a7a5c57127b0ad9f6402f254c30fcf2f1635
CarpeDiEmma <ejroud@gmail.com>
2015-03-19 19:22:03 -0400

    Fix Deprecation Bug and Removed g_DescriptorJSON as global

--- src/GUI/lookup_table.js
+++ src/GUI/lookup_table.js
@@ -1,53 +1,60 @@
 // File Name:               lookup_table.js
 // Description:             Generates lookup table
 // Date Created:            3/5/2015
-// Contributors:            Emma Roudabush, Vanajam Soni
+// Contributors:            Emma Roudabush, Vanajam Soni, Paul Jang
 // Date Last Modified:      3/19/2015
 // Last Modified By:        Emma Roudabush
 // Dependencies:            descriptor.php, data.js
 // Additional Notes:        N/A
 
 function CreateLookupTable ()
-{
-    GetDescriptor();
-	console.log(g_DescriptorJSON);
-    GenerateLookupTable();
-    GenerateStatReferenceList();
-	console.log(g_StatList); 
-	console.log(g_LookupTable);
+{	
+	$.when(GetDescriptor()).done(function(data){
+		var DescriptorJSON = data;
+		GenerateLookupTable(DescriptorJSON);
+		GenerateStatReferenceList(DescriptorJSON);
+		console.log(g_StatList); 
+		console.log(g_LookupTable);
+	});
 }
 
-function GetDescriptor ()
+function GetDescriptor()
 {
-	$.ajax({                                      
+	return $.ajax({                                      
 		url: 'http://usve74985.serverprofi24.com/API/descriptor.php',                                                     
 		dataType: 'JSON',                 
-		async: false,
 		success: function(data){     
 			console.log("Successfully received descriptor.php");
-			g_DescriptorJSON = data;
 		} 
 	});
 }
 
-function GenerateLookupTable()
+function GenerateLookupTable(DescriptorJSON)
 {
-    g_LookupTable = new Array(g_DescriptorJSON.cc2.length);
-    for (i = 0; i < g_DescriptorJSON.cc2.length; i++)
+    g_LookupTable = new Array(DescriptorJSON.cc2.length);
+    for (i = 0; i < DescriptorJSON.cc2.length; i++)
     {
 		g_LookupTable[i] = new Array(3);
-        g_LookupTable[i][0] = g_DescriptorJSON.cc2[i];
-        g_LookupTable[i][1] = g_DescriptorJSON.common_name[i];
+        g_LookupTable[i][0] = DescriptorJSON.cc2[i];
+        g_LookupTable[i][1] = DescriptorJSON.common_name[i];
         g_LookupTable[i][2] = 0;
     }
 }
 
-function GenerateStatReferenceList()
+function GenerateStatReferenceList(DescriptorJSON)
 {
-	g_StatList = []; 
-	for (i = 0; i < g_DescriptorJSON.stats.length; i++)
+	g_StatList = new Array(DescriptorJSON.stats.length); 
+	for (i = 0; i < DescriptorJSON.stats.length; i++)
 	{
-		g_StatList[i] = g_DescriptorJSON.stats[i];
+		g_StatList[i] = DescriptorJSON.stats[i];
+	}
+}
+
+function SetHMS(hms)
+{
+	for (i = 0; i < g_LookupTable.length; i++)
+	{
+		
 	}
 }
 
