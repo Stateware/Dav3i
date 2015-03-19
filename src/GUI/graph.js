0ceacf0156db5b05180ae4704c975ba2888729b4
Berty Ruan <berty.ruan@gmail.com>
2015-03-19 19:53:46 -0400

    Began testing Google Charts API functionality

--- src/GUI/graph.js
+++ src/GUI/graph.js
@@ -1,19 +1,48 @@
 ﻿// File Name: graph.js
 // Description: This file generates graphs based on parsed data
 // Date Created: 3/17/2015
-// Contributors: Nicholas Dyszel
-// Date Last Modified: 3/17/2015
-// Last Modified By: Nicholas Dyszel
-// Dependencies: [[ graphing API ]]
+// Contributors: Nicholas Dyszel, Berty Ruan, Arun Kumar
+// Date Last Modified: 3/19/2015
+// Last Modified By: Nicholas Dyszel, Berty Ruan, Arun Kumar
+// Dependencies: client_parser.js, ..., [Google Charts API]
 // Additional Notes: N/A
 
 // Author: Nicholas Dyszel
 // Date Created: 3/17/2015
 // Last Modified: 3/17/2015 by Nicholas Dyszel
-// Description: Takes 1 stat ID and stat data and generates a graph
+// Description: Takes 1 stat ID and stat data and generates a graph (Google Charts API)
 // PRE:
 // POST:
-function GenerateSingle(statID, data) {
+function GenerateSingle(statID, data) 
+{
+	//input comes from client_parser.js
+	//statID -> String
+	//data   -> Array of floats
+	google.load("visualization", "1", {packages:["corechart"]});
+	google.setOnLoadCallback(drawVisualization);
+
+	function drawVisualization() {
+	  // Some raw data (not necessarily accurate)
+	  var data = google.visualization.arrayToDataTable([
+	    ['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda', 'Average'],
+	    ['2004/05',  165,      938,         522,             998,           450,      614.6],
+	    ['2005/06',  135,      1120,        599,             1268,          288,      682],
+	    ['2006/07',  157,      1167,        587,             807,           397,      623],
+	    ['2007/08',  139,      1110,        615,             968,           215,      609.4],
+	    ['2008/09',  136,      691,         629,             1026,          366,      569.6]
+	  ]);
+
+	  var options = {
+	    title : 'Monthly Coffee Production by Country',
+	    vAxis: {title: "Cups"},
+	    hAxis: {title: "Month"},
+	    seriesType: "bars",
+	    series: {5: {type: "line"}}
+	  };
+
+	  var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+	  chart.draw(data, options);
+
 
 }
 
