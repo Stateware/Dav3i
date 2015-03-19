4ed2ac221750c5c504a104acd00d6c1127e5b783
CarpeDiEmma <ejroud@gmail.com>
2015-03-19 19:50:08 -0400

    Completed function headers for Lookup_Table.js

--- src/GUI/lookup_table.js
+++ src/GUI/lookup_table.js
@@ -1,14 +1,14 @@
-// File Name:               lookup_table.js
-// Description:             Generates lookup table
-// Date Created:            3/5/2015
-// Contributors:            Emma Roudabush, Vanajam Soni, Paul Jang, Joshua Crafts
-// Date Last Modified:      3/19/2015
-// Last Modified By:        Emma Roudabush
-// Dependencies:            descriptor.php, data.js
-// Additional Notes:        N/A
+// File Name:				lookup_table.js
+// Description:				Generates lookup table
+// Date Created:			3/5/2015
+// Contributors:			Emma Roudabush, Vanajam Soni, Paul Jang, Joshua Crafts
+// Date Last Modified:		3/19/2015
+// Last Modified By:		Emma Roudabush
+// Dependencies:			descriptor.php, data.js
+// Additional Notes:		N/A
 
 // Author: Emma Roudabush
-// Date Created: 
+// Date Created: 3/5/2015
 // Last Modified: 3/19/2015 by Emma Roudabush
 // Description: Fills g_DescriptorJSON with the contents of descriptor.php,
 //				fills g_LookupTable and g_StatList with their corresponding data
@@ -27,24 +27,24 @@ function CreateLookupTable ()
 }
 
 // Author: Emma Roudabush
-// Date Created:
+// Date Created: 3/5/2015
 // Last Modified: 3/19/2015 by Emma Roudabush
 // Description: Retrieves descriptor.php from the server				
 // PRE: descriptor.php exists on the server.
 // POST: returns the contents of descriptor.php
 function GetDescriptor()
 {
-	return $.ajax({                                      
-		url: 'http://usve74985.serverprofi24.com/API/descriptor.php',                                                     
-		dataType: 'JSON',                 
-		success: function(data){     
+	return $.ajax({										 
+		url: 'http://usve74985.serverprofi24.com/API/descriptor.php',													  
+		dataType: 'JSON',				  
+		success: function(data){	 
 			console.log("Successfully received descriptor.php");
 		} 
 	});
 }
 
 // Author: Emma Roudabush
-// Date Created: 
+// Date Created: 3/5/2015
 // Last Modified: 3/19/2015 by Emma Roudabush
 // Description: Fills the contents of g_LookupTable with data from
 //				DescriptorJSON. This includes the CC2 codes, the 
@@ -54,25 +54,24 @@ function GetDescriptor()
 // POST: g_LookupTable has the correct CC2, name, and HMS values
 function GenerateLookupTable(DescriptorJSON)
 {
-    g_LookupTable = new Array(DescriptorJSON.cc2.length);
-    for (i = 0; i < DescriptorJSON.cc2.length; i++)
-    {
+	g_LookupTable = new Array(DescriptorJSON.cc2.length);
+	for (i = 0; i < DescriptorJSON.cc2.length; i++)
+	{
 		g_LookupTable[i] = new Array(3);
-        g_LookupTable[i][0] = DescriptorJSON.cc2[i];
-        g_LookupTable[i][1] = DescriptorJSON.common_name[i];
-        g_LookupTable[i][2] = 0;
-    }
+		g_LookupTable[i][0] = DescriptorJSON.cc2[i];
+		g_LookupTable[i][1] = DescriptorJSON.common_name[i];
+		g_LookupTable[i][2] = 0;
+	}
 }
 
 // Author: Emma Roudabush
-// Date Created: 
+// Date Created: 3/5/2015
 // Last Modified: 3/19/2015 by Emma Roudabush
 // Description: Fills the contents of g_StatList with the stats
 //				data from DescriptorJSON			
 // PRE: g_DescriptorJSON exists with the correct data from descriptor.php,
 //		g_StatList exists
 // POST: g_StatList has the correct stat values
-
 function GenerateStatReferenceList(DescriptorJSON)
 {
 	g_StatList = new Array(DescriptorJSON.stats.length); 
@@ -83,11 +82,11 @@ function GenerateStatReferenceList(DescriptorJSON)
 }
 
 // Author: Emma Rouabush
-// Date Created:
-// Last Modified: 3/19/2015
-// Description:
-// PRE: 
-// POST:
+// Date Created: 3/17/2015
+// Last Modified: 3/19/2015 by Emma Roudabush
+// Description:Replace HMS values in lookup table with new HMS data (will happen just after lookup table generation for default HMS)
+// PRE: hms contains valid heat map values and hms is of size g_LookupTable.length
+// POST: g_LookupTable has heat map values of hms
 function SetHMS(hms)
 {
 	for (i = 0; i < g_LookupTable.length; i++)
@@ -97,11 +96,11 @@ function SetHMS(hms)
 }
 
 // Author: Emma Rouabush
-// Date Created:
-// Last Modified: 3/19/2015
-// Description:
-// PRE: 
-// POST:
+// Date Created: 3/17/2015
+// Last Modified: 3/19/2015 by Emma Roudabush
+// Description: Translate CC2 to CID using g_LookupTable
+// PRE: cc2 is sorted
+// POST: Correct CID(s) are returned
 function GetCID(cc2)
 {
 	var cids = new Array();
