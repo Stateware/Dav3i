e7fedeab30d6fad30e804df2ea45499c19da123b
William Bittner <wjb5174@psu.edu>
2015-03-24 19:47:31 -0400

    Add error handling, comments, and more structure

--- src/API/data_parser.php
+++ src/API/data_parser.php
@@ -1,41 +1,63 @@
 <?php
 
 require_once("connect.php");
+require_once("toolbox.php");
 
-$testData = "";
-$fileTemporaryName = $_FILES['userfile']['tmp_name'];
-$testData = file_get_contents($fileTemporaryName);
 
-$testData = preg_replace("/\,NA\,|,,/", ",-1,", preg_replace("/\,NA\,|,,/", ",-1,", $testData));
 
 $databaseConnection = GetDatabaseConnection();
 
+//Get CSV from data_uploader, then format it correctly with regex
+//Check for uploading error
+$fileError = $_FILES['userfile']['error'];
+if($fileError !== UPLOAD_ERR_OK)
+{
+	ThrowFatalError("Error Uploading - Please Try Again");
+}
+//Check to make sure file is a CSV
+$fileType = $_FILES['userfile']['type'];
+if($fileType != "text/csv")
+{
+	ThrowFatalError("Incorrect Data Type of Uploaded File - Please Use CSV");
+}
+//Format string
+$fileTemporaryName = $_FILES['userfile']['tmp_name'];
+$testData = file_get_contents($fileTemporaryName);
+$testData = preg_replace("/\,NA\,|,,/", ",-1,", preg_replace("/\,NA\,|,,/", ",-1,", $testData));
+
+//Get data from inputter about what the data corresponds to in the database 
 $type = $_POST['data_type'];
 $tableName = $_POST['table_name'];
 $statisticName = $_POST['stat_name'];
 
+//Query to insert the stat's meta data into the meta table 
 $metaTableQuery = "INSERT INTO meta_stats (stat_name, table_name) VALUES ('$statisticName', '$tableName')";
 $metaTableResults = $databaseConnection->query($metaTableQuery);
 if ($metaTableResults === false)
 {
-    echo "Query failed.";
-    die();
+    ThrowFatalError("Meta Data Query Failed");
 }
 
+//Replace all newlines with a '|' so that we can parse it easier
 $testData = preg_replace('~\R~', "|", $testData);
 
+//Create array separated with each row in its own index
 $queryArray = array();
 $tableArray = explode("|", $testData);
+//Separating the first entry from all the other
 foreach($tableArray as $value)
 {
     $tempArray = explode(",", $value, 2);
     $queryArray[$tempArray[0]] = $tempArray[1];
 }
 
+//Delete the first element, then assign to headers
 $headers = array_shift($queryArray);
 
+//Separate the header fields
 $columnsArray = explode(",", $headers);
 
+//Create the query to create the query from the headers
 $createTableQuery = "CREATE TABLE $tableName (`country_id` int(10)";
 foreach ($columnsArray as $column)
 {
@@ -43,12 +65,10 @@ foreach ($columnsArray as $column)
     $createTableQuery .= ", `$fixedColumnName` $type";
 }
 $createTableQuery .= ");";
-
 $createTableResults = $databaseConnection->query($createTableQuery);
 if ($createTableQuery === false)
 {
-    echo "Query failed.";
-    die();
+    ThrowFatalError("Table Creation Query Failed");
 }
 
 // Create lookup table
@@ -60,9 +80,7 @@ while($countryLookupRow = $countryLookupResults->fetch_assoc())
     $countryLookupTable[$countryLookupRow['cc3']] = $countryLookupRow['country_id'];
 }
 
-
-$specificQuery = "";
-
+//Insert the actual data into the database
 foreach($queryArray as $country => $query)
 {
 	$specificQuery = "INSERT INTO $tableName VALUES(";
@@ -76,13 +94,8 @@ foreach($queryArray as $country => $query)
 	$specificResults = $databaseConnection->query($specificQuery);
 	if ($specificResults === false)
 	{
-		echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!query didn't work<br />";
+		ThrowInconvenientError("You're missing a row - refer to the database for specifics.");
 	}
-	else
-	{
-		echo "query definately passed.<br />";
-	}
-}
 //ALTER TABLE data_cases ORDER BY country_id ASC
 
 ?>
