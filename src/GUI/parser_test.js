eef70880289269fd39b5857dde6d5f7c2c266f0b
Nicholas Denaro <NicholasCDenaro@aol.com>
2015-03-26 18:42:38 -0400

    Removed unused comments from the parser test

--- src/GUI/parser_test.js
+++ src/GUI/parser_test.js
@@ -18,55 +18,6 @@ function RunParserTest()
 {
     // flags that indicates whether or not the JSON and output arrays match
     var match1 = true; var match2 = true; var match3 = true;
-    /*var data1 =                         // JSON of random    numbers
-    {
-    1:
-    {
-       1: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
-       2: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
-       3: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)]
-    },
-    };
-
-    var data2 =                         // JSON of random numbers
-    {
-    1:
-    {
-        1: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
-        2: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
-        3: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)]
-        },
-    2:
-    {
-        1: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
-        2: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
-        3: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)]
-    }
-    };
-
-    var data3 =                         // JSON of random numbers
-    {
-    1:
-    {
-        1: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
-        2: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
-        3: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)]
-    },
-    2:
-    {
-        1: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
-        2: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
-        3: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)]
-    },
-    3:
-    {
-        1: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
-        2: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
-        3: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)]
-    }
-    };
-    var cids1 = [1]; var cids2 = [2,1]; var cids3 = [3,1,2];
-    */
 
     var cid1 = Math.floor(Math.random() * 100);
     var data1 = { cid: new Array() };
@@ -82,45 +33,8 @@ function RunParserTest()
             expectedOutput1[stat][year] = randomValue;
         }
     }
-
-    //alert(JSON.stringify(data1,"\n",4));
-    //alert(JSON.stringify(expectedOutput1,"\n",4));
     
     var outputArray1 = ParseData(data1); // fills outputArray with parsed JSON data
-    //var outputArray2 = ParseData(data2);
-    //var outputArray3 = ParseData(data3);
-
-    // checks through the JSON and output array to see if all the elements match
-    //Loops through the countries in the JSON
-    /*for (var cid = 0; cid < cids1.length ; cid++) {
-        //Loops through the stats in the JSON
-        for (var stat = 1; data1[cids1[cid]].hasOwnProperty(stat) ; stat++) {
-            if (outputArray1[stat - 1][cid] != data1[cids1[cid]][stat]) {
-                match1 = false;
-            }
-        }
-    }
-
-    for (var cid = 0; cid < cids2.length ; cid++) {
-        //Loops through the stats in the JSON
-        for (var stat = 1; data2[cids2[cid]].hasOwnProperty(stat) ; stat++) {
-            if (outputArray2[stat - 1][cid] != data2[cids2[cid]][stat]) {
-                match2 = false;
-            }
-        }
-    }
-
-    for (var cid = 0; cid < cids3.length ; cid++) {
-        //Loops through the stats in the JSON
-        for (var stat = 1; data3[cids3[cid]].hasOwnProperty(stat) ; stat++) {
-            if (outputArray3[stat - 1][cid] != data3[cids3[cid]][stat]) {
-                match3 = false;
-            }
-        }
-    }*/
-
-    //alert(JSON.stringify(expectedOutput1, "\n", 4));
-   // alert(JSON.stringify(outputArray1,"\n",4));
 
     for (var stat = 0; match1 && stat < 10; stat++)
     {
