1dc523f83bff11142ff32e94b95a8c7f85e60e41
CarpeDiEmma <ejroud@gmail.com>
2015-03-26 18:51:55 -0400

    Created settings.js

---
+++ src/GUI/settings.js
@@ -0,0 +1,9 @@
+// File Name:               settings.js
+// Description:             Module to modify and work with settings
+// Date Created:            3/26/2015
+// Contributors:            Emma Roudabush, Paul Jang
+// Date Last Modified:      3/26/2015
+// Last Modified By:        Emma Roudabush, Paul Jang
+// Dependencies:            data.js, index.html
+// Additional Notes:        N/A
+
