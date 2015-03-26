78cc06ada4748ce588eee77bf96e6c0dca6560c2
Paul Jang <pjang94@gmail.com>
2015-03-26 19:27:23 -0400

    created dynamic_markup.js
    dynamic_markup contains code needed to dynamically create divs in the
    markup

---
+++ src/GUI/dynamic_markup.js
@@ -0,0 +1,32 @@
+// File Name:               dynamic_markup.js
+// Description:             This module contains the code needed to dynamically create modules on the client
+// Date Created:            3/26/2015
+// Contributors:            Paul Jang, Nicholas Denaro
+// Date Last Modified:      3/26/2015
+// Last Modified By:        Paul Jang
+// Dependencies:            index.html, lookup_table.js
+// Additional Notes:        N/A
+
+// Author: Paul Jang, Nicholas Denaro
+// Date Created: 3/26/2015
+// Last Modified: 3/26/2015 by Paul Jang
+// Description: Retrieve stat values from the lookup_table,
+//				Builds the tabs and inserts them into index.html
+// PRE: lookup_table is filled correctly, index.html exists
+// POST: index.html contains tabs of the correct stat data from the lookup_table
+function BuildTabs()
+{
+	
+}
+
+// Author: Paul Jang, Nicholas Denaro
+// Date Created: 3/26/2015
+// Last Modified: 3/26/2015 by Paul Jang
+// Description: build divs where the graphs go in index.html
+// PRE: Called from BuildTabs
+// POST: appropriate divs are created
+function BuildDivs()
+{
+
+}
+
