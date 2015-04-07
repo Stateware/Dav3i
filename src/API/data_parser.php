fad8a813dd0309d7d5d450c3f9a2b88a664555af
Drew Lopreiato <drew.lopreiato@gmail.com>
2015-04-07 18:12:31 -0400

    Data parser fixings

--- src/API/data_parser.php
+++ src/API/data_parser.php
@@ -1,63 +1,48 @@
 <?php
 
 require_once("connect.php");
-require_once("toolbox.php");
 
+$testData = "";
+$fileTemporaryName = $_FILES['userfile']['tmp_name'];
+$testData = file_get_contents($fileTemporaryName);
 
+$testData = preg_replace("/,,/", ",-1,", preg_replace("/,,/", ",-1,", $testData));
+$testData = preg_replace("/,NA/", ",-1", preg_replace("/,NA/", ",-1", $testData));
+$testData = preg_replace("/\"/", "", $testData);
 
 $databaseConnection = GetDatabaseConnection();
 
-//Get CSV from data_uploader, then format it correctly with regex
-//Check for uploading error
-$fileError = $_FILES['userfile']['error'];
-if($fileError !== UPLOAD_ERR_OK)
-{
-	ThrowFatalError("Error Uploading - Please Try Again");
-}
-//Check to make sure file is a CSV
-$fileType = $_FILES['userfile']['type'];
-if($fileType != "text/csv")
-{
-	ThrowFatalError("Incorrect Data Type of Uploaded File - Please Use CSV");
-}
-//Format string
-$fileTemporaryName = $_FILES['userfile']['tmp_name'];
-$testData = file_get_contents($fileTemporaryName);
-$testData = preg_replace("/\,NA\,|,,/", ",-1,", preg_replace("/\,NA\,|,,/", ",-1,", $testData));
-
-//Get data from inputter about what the data corresponds to in the database 
 $type = $_POST['data_type'];
 $tableName = $_POST['table_name'];
 $statisticName = $_POST['stat_name'];
 
-//Query to insert the stat's meta data into the meta table 
 $metaTableQuery = "INSERT INTO meta_stats (stat_name, table_name) VALUES ('$statisticName', '$tableName')";
-$metaTableResults = $databaseConnection->query($metaTableQuery);
+/*$metaTableResults = $databaseConnection->query($metaTableQuery);
 if ($metaTableResults === false)
 {
-    ThrowFatalError("Meta Data Query Failed");
-}
+    echo "Query failed.";
+    die();
+}*/
 
-//Replace all newlines with a '|' so that we can parse it easier
-$testData = preg_replace('~\R~', "|", $testData);
+//$testData = preg_replace('~\R~', "|", preg_replace("/[\r\n]/", "|", $testData));
+$testData = trim(preg_replace('~\R~', "|", $testData));
 
-//Create array separated with each row in its own index
 $queryArray = array();
 $tableArray = explode("|", $testData);
-//Separating the first entry from all the other
 foreach($tableArray as $value)
 {
+    
     $tempArray = explode(",", $value, 2);
-    $queryArray[$tempArray[0]] = $tempArray[1];
+    if (isset($tempArray[1]))
+    {
+        $queryArray[$tempArray[0]] = $tempArray[1];
+    }
 }
 
-//Delete the first element, then assign to headers
 $headers = array_shift($queryArray);
 
-//Separate the header fields
 $columnsArray = explode(",", $headers);
 
-//Create the query to create the query from the headers
 $createTableQuery = "CREATE TABLE $tableName (`country_id` int(10)";
 foreach ($columnsArray as $column)
 {
@@ -65,11 +50,13 @@ foreach ($columnsArray as $column)
     $createTableQuery .= ", `$fixedColumnName` $type";
 }
 $createTableQuery .= ");";
-$createTableResults = $databaseConnection->query($createTableQuery);
+
+/*$createTableResults = $databaseConnection->query($createTableQuery);
 if ($createTableQuery === false)
 {
-    ThrowFatalError("Table Creation Query Failed");
-}
+    echo "Query failed.";
+    die();
+}*/
 
 // Create lookup table
 $countryLookupTable = array();
@@ -80,22 +67,54 @@ while($countryLookupRow = $countryLookupResults->fetch_assoc())
     $countryLookupTable[$countryLookupRow['cc3']] = $countryLookupRow['country_id'];
 }
 
-//Insert the actual data into the database
+
+$specificQuery = "";
+
 foreach($queryArray as $country => $query)
 {
 	$specificQuery = "INSERT INTO $tableName VALUES(";
     
-    $specificQuery .= $countryLookupTable[$country] . ", ";
+    $specificQuery .= $countryLookupTable[$country];
 
-    $specificQuery .= $query;
+    $allTheStats = explode(",", $query);
+    foreach ($allTheStats as $statistic) {
+        $specificQuery .= sprintf(", %f", ScientificConversion($statistic));
+    }
+
+    //$specificQuery .= $query;
 
   	$specificQuery .= ");";
 	
-	$specificResults = $databaseConnection->query($specificQuery);
+    echo $specificQuery;
+	/*$specificResults = $databaseConnection->query($specificQuery);
 	if ($specificResults === false)
 	{
-		ThrowInconvenientError("You're missing a row - refer to the database for specifics.");
+		echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!query didn't work<br />";
 	}
-//ALTER TABLE data_cases ORDER BY country_id ASC
+	else
+	{
+		echo "query definately passed.<br />";
+	}*/
+}
+//ALTER TABLE data_cases ORDER BY country_id ASC//*/
+
+// convert text in scientific notation into a floating point value
+// Steven Shaffer, March 30, 2015
+function ScientificConversion($in) {
+    $pos = strpos($in, "E");
+    if ($pos === false) {
+        //Just convert it directly
+        return floatval($in);
+    }
+    else {
+        //Needs to be converted
+        $part1 = substr($in, 0, $pos);
+        $part2 = substr($in, $pos+1);
+        $base = $part1 + 0;
+        $exponent = $part2 + 0;
+        $answer = $base * (pow(10, $exponent));
+        return $answer;
+    }
+}
 
 ?>
