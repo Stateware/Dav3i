79d3e87a77920dcec1905785b687e623f77dcc35
Drew Lopreiato <drew.lopreiato@gmail.com>
2015-04-07 18:46:24 -0400

    Document api library and use proper white space

--- src/API/api_library.php
+++ src/API/api_library.php
@@ -17,186 +17,214 @@ require_once("connect.php");
 
 function ByStat($statID, $year)
 {
-	$heatMapArray = array();
-	$descriptor = Descriptor();
-	$yearRange = $descriptor['yearRange'];
-	$numStats = count($descriptor['stats']);
-	$databaseConnection = GetDatabaseConnection();
-	
-	//if year is set to default, there was no year enter and thus we give it the most recent year
-	//this also means we don't need to check to see if the year is valid or sanitary 
-	if($year == DEFAULT_STRING)
-	{
-		$year = $yearRange[1];
-	}
-	else
-	{
-		//check inputs are sanitary
-		if (!IsSanitaryYear($year))
-		{
-	    	ThrowFatalError("Input is unsanitary: year");
-		}
+    $heatMapArray = array();
+    $descriptor = Descriptor();
+    $yearRange = $descriptor['yearRange'];
+    $numStats = count($descriptor['stats']);
+    $databaseConnection = GetDatabaseConnection();
+    
+    //if year is set to default, there was no year enter and thus we give it the most recent year
+    //this also means we don't need to check to see if the year is valid or sanitary 
+    if($year == DEFAULT_STRING)
+    {
+        $year = $yearRange[1];
+    }
+    else
+    {
+        //check inputs are sanitary
+        if (!IsSanitaryYear($year))
+        {
+            ThrowFatalError("Input is unsanitary: year");
+        }
 
-		//Check inputs are valid
-		if (!IsValidYear($year, $yearRange))
-		{
-		    ThrowFatalError("Input is invalid: year");
-		}
-	}
-	//check inputs are sanitary
-	if (!IsSanitaryStatID($statID))
-	{
-	   	ThrowFatalError("Input is unsanitary: statID");
-	}
-	
-	//Check inputs are valid
-	if (!IsValidStatID($statID,$numStats))
-	{
-	   ThrowFatalError("Input is invalid: statID");
-	}
-	
-	// The statID given to us is expected to be indexed by 0, however our database is indexed by 1, so we'll add 1
-	$databaseIndexedStatID = $statID + 1;
-	
-	// Retrieve the table name of the inputted stat ID.
-	$tableName = GetFirstRowFromColumn($databaseConnection, "meta_stats", "table_name", "table_id = $databaseIndexedStatID");
-	$heatMapQuery = "SELECT `" . $year . "` FROM " . $tableName . " ORDER BY country_id ASC";
-	$heatMapResults = $databaseConnection->query($heatMapQuery);
-	while($heatMapRow = $heatMapResults->fetch_assoc())
-	{
-   		array_push($heatMapArray, $heatMapRow[$year]);
-	}
-	// if we don't return an array with more than one element in it, it'll come out not as an object, but as an array
-	// this line of code forces the json_decode to output the data as an object.
-	$containingArray = array($statID => $heatMapArray, 'force' => "object");
+        //Check inputs are valid
+        if (!IsValidYear($year, $yearRange))
+        {
+            ThrowFatalError("Input is invalid: year");
+        }
+    }
+    //check inputs are sanitary
+    if (!IsSanitaryStatID($statID))
+    {
+           ThrowFatalError("Input is unsanitary: statID");
+    }
+    
+    //Check inputs are valid
+    if (!IsValidStatID($statID,$numStats))
+    {
+       ThrowFatalError("Input is invalid: statID");
+    }
+    
+    // The statID given to us is expected to be indexed by 0, however our database is indexed by 1, so we'll add 1
+    $databaseIndexedStatID = $statID + 1;
+    
+    // Retrieve the table name of the inputted stat ID.
+    $tableName = GetFirstRowFromColumn($databaseConnection, "meta_stats", "table_name", "table_id = $databaseIndexedStatID");
+    $heatMapQuery = "SELECT `" . $year . "` FROM " . $tableName . " ORDER BY country_id ASC";
+    $heatMapResults = $databaseConnection->query($heatMapQuery);
+    while($heatMapRow = $heatMapResults->fetch_assoc())
+    {
+           array_push($heatMapArray, $heatMapRow[$year]);
+    }
+    // if we don't return an array with more than one element in it, it'll come out not as an object, but as an array
+    // this line of code forces the json_decode to output the data as an object.
+    $containingArray = array($statID => $heatMapArray, 'force' => "object");
 
-	return ($containingArray);	
+    return ($containingArray);    
 }
 
 
 function ByCountry($countryIDs)
 {
-	$databaseConnection = GetDatabaseConnection();
-	$byCountryArray = array();
-	$descriptor = Descriptor();
-	$numCountries = count($descriptor['common_name']);
-	$statTables = array();
-	
-	if (!IsSanitaryCountryList($countryIDs))
-	{
-		ThrowFatalError("Input is unsanitary: countryIDs");
-	}
-	
-	$countryIDList = explode(",", $countryIDs);
-	$databaseIndexedCountryIDList = array();
+    $databaseConnection = GetDatabaseConnection();
+    $byCountryArray = array();
+    $descriptor = Descriptor();
+    $numCountries = count($descriptor['common_name']);
+    $statTables = array();
+    
+    if (!IsSanitaryCountryList($countryIDs))
+    {
+        ThrowFatalError("Input is unsanitary: countryIDs");
+    }
+    
+    $countryIDList = explode(",", $countryIDs);
+    $databaseIndexedCountryIDList = array();
 
-	// validate each countryID
-	foreach ($countryIDList as $countryID)
-	{
-		if (!IsValidCountryID($countryID, $numCountries))
-		{
-			ThrowFatalError("Input is invalid: countryID (" . $countryID . ")");
-		}
-	}
-	
-	// The countryIDs given to us is expected to be indexed by 0, however our database is indexed by 1, so we add 1
-	foreach ($countryIDList as $countryID)
-	{
-		array_push($databaseIndexedCountryIDList, $countryID + 1);
-	}
+    // validate each countryID
+    foreach ($countryIDList as $countryID)
+    {
+        if (!IsValidCountryID($countryID, $numCountries))
+        {
+            ThrowFatalError("Input is invalid: countryID (" . $countryID . ")");
+        }
+    }
+    
+    // The countryIDs given to us is expected to be indexed by 0, however our database is indexed by 1, so we add 1
+    foreach ($countryIDList as $countryID)
+    {
+        array_push($databaseIndexedCountryIDList, $countryID + 1);
+    }
 
-	// put each stat table into an array, where its index is the id for that table
-	$statTablesQuery = "SELECT table_id, table_name FROM meta_stats";
-	$statTablesResults = $databaseConnection->query($statTablesQuery);
-	while ($statTablesRow = $statTablesResults->fetch_assoc())
-	{
-	    $statTables[$statTablesRow['table_id']] = $statTablesRow['table_name'];
-	}
-	
-	// get the queries for each country and stat
-	$countryDataQueries = GetCountryQueries($statTables, $databaseIndexedCountryIDList);
-	
-	// query the query for each country n stuff.
-	foreach($countryDataQueries as $statID => $query)
-	{
-		$countryDataResults = $databaseConnection->query($query);
-	    while ($countryDataRow = $countryDataResults->fetch_array(MYSQLI_NUM))
-	    {
-	    	$countryID = $countryDataRow[0];
-	    	$byCountryArray[$countryID - 1][$statID - 1] = array_slice($countryDataRow, 1);
-	    }	
-	}
-	
-	// if we don't return an array with more than one element in it, it'll come out not as an object, but as an array
-	// this line of code forces the json_decode to output the data as an object.
-	$byCountryArray['force'] = "object";
+    // put each stat table into an array, where its index is the id for that table
+    $statTablesQuery = "SELECT table_id, table_name FROM meta_stats";
+    $statTablesResults = $databaseConnection->query($statTablesQuery);
+    while ($statTablesRow = $statTablesResults->fetch_assoc())
+    {
+        $statTables[$statTablesRow['table_id']] = $statTablesRow['table_name'];
+    }
+    
+    // get the queries for each country and stat
+    $countryDataQueries = GetCountryQueries($statTables, $databaseIndexedCountryIDList);
+    
+    // query the query for each country n stuff.
+    foreach($countryDataQueries as $statID => $query)
+    {
+        $countryDataResults = $databaseConnection->query($query);
+        while ($countryDataRow = $countryDataResults->fetch_array(MYSQLI_NUM))
+        {
+            $countryID = $countryDataRow[0];
+            $byCountryArray[$countryID - 1][$statID - 1] = array_slice($countryDataRow, 1);
+        }    
+    }
+    
+    // if we don't return an array with more than one element in it, it'll come out not as an object, but as an array
+    // this line of code forces the json_decode to output the data as an object.
+    $byCountryArray['force'] = "object";
 
-	return $byCountryArray;
+    return $byCountryArray;
 }
 
 
 function Descriptor()
 {
-	$databaseConnection = GetDatabaseConnection();
-	$cc2 = array();
-	$cc3 = array();
-	$countryName = array();
-	//Hardcoded table because idk
-	$yearRange = GetYearRange($databaseConnection, "data_births");
-	$stats = GetStatNames($databaseConnection);
-	
-	$countriesQuery = "SELECT * FROM meta_countries ORDER BY country_id";
-	$countriesResult = $databaseConnection->query($countriesQuery);
-	
-	//fill cc2, cc3, and countryName arrays in order by ID
-	while($countriesRow = $countriesResult->fetch_assoc())
-	{
-		array_push($cc2, $countriesRow['cc2']);
-		array_push($cc3, $countriesRow['cc3']);
-		array_push($countryName, $countriesRow['common_name']);
-	}
-	
-	//return the nested arrays
-	return(array("yearRange" => $yearRange, "cc2" => $cc2, "cc3"=> $cc3, "common_name" => $countryName, "stats" => $stats));
+    $databaseConnection = GetDatabaseConnection();
+    $cc2 = array();
+    $cc3 = array();
+    $countryName = array();
+    //Hardcoded table because idk
+    $yearRange = GetYearRange($databaseConnection, "data_births");
+    $stats = GetStatNames($databaseConnection);
+    
+    $countriesQuery = "SELECT * FROM meta_countries ORDER BY country_id";
+    $countriesResult = $databaseConnection->query($countriesQuery);
+    
+    //fill cc2, cc3, and countryName arrays in order by ID
+    while($countriesRow = $countriesResult->fetch_assoc())
+    {
+        array_push($cc2, $countriesRow['cc2']);
+        array_push($cc3, $countriesRow['cc3']);
+        array_push($countryName, $countriesRow['common_name']);
+    }
+    
+    //return the nested arrays
+    return(array("yearRange" => $yearRange, "cc2" => $cc2, "cc3"=> $cc3, "common_name" => $countryName, "stats" => $stats));
 }
 
 // ======================= Helper Functions =======================
- 
+
+// Author:        William Bittner, Andrew Lopreiato
+// Date Created:  2/22/2015
+// Last Modified: 4/7/2015 by Andrew Lopreiato
+// Description:   This function takes any input and determines if it is safe to
+//                send to the server in the context of a year.
 function IsSanitaryYear($year)
+// PRE:  year is anything
+// POST: true if year is in the format of 4 digits in a row.
 {
-    // This matches exclusively 4 digits in a row.
     return preg_match("/^\d{4}$/", $year) === 1;
 }
-
+// Author:        William Bittner, Andrew Lopreiato
+// Date Created:  2/22/2015
+// Last Modified: 4/7/2015 by Andrew Lopreiato
+// Description:   This function takes any input and determines if it is safe to
+//                send to the server in the context of a statistic ID.
 function IsSanitaryStatID($statID)
+// PRE:  statID is anything
+// POST: true if the statID is 1 or more digits in a row
 {
-    // This matches exclusively 1 or more digits in a row.
     return preg_match("/^\d+$/", $statID) === 1;
 }
 
+// Author:        William Bittner, Andrew Lopreiato
+// Date Created:  2/22/2015
+// Last Modified: 4/7/2015 by Andrew Lopreiato
+// Description:   This function takes any input and determines if it is safe to
+//                send to the server in the context of a country list.
 function IsSanitaryCountryList($countryList)
+// PRE:  countryList is anything
+// POST: true if the countryList is 1 or more groups of comma delimited digits.
 {
-	// This matches exclusively 1 or more groups of comma delimited digits.
-	return preg_match("/^\d+(,\d+)*$/", $countryList) === 1;
+    return preg_match("/^\d+(,\d+)*$/", $countryList) === 1;
 }
 
+// Author:        William Bittner, Andrew Lopreiato
+// Date Created:  2/22/2015
+// Last Modified: 4/7/2015 by Andrew Lopreiato
+// Description:   This function takes a year, and determines if it falls within
+//                a given year range (in the form of a 2 element array).
 function IsValidYear($year, $yearRange)
+// PRE:  year is a number, and yearRange is an array with two elements which are numbers
+// POST: true if the year falls within the given year range
 {
     return ($year >= $yearRange[0] && $year <= $yearRange[1]);
 }
 
+// Author:        William Bittner, Andrew Lopreiato
+// Date Created:  2/22/2015
+// Last Modified: 4/7/2015 by Andrew Lopreiato
+// Description:   This function determines if a statistic ID falls within the valid statID range.
 function IsValidStatID($statID, $numStats)
+// PRE:  statID is a number, and numStats is a number
+// POST: true if statID falls between 0 and numStats
 {
-	//As stats are numbered starting at one, the count of the array is equal to the highest value stat.
-	//Check >0 and <= the number of stats as a stat id can't be less than 1 or more than the number of stats
     return($statID < $numStats && $statID >= 0);
 }
 
 function IsValidCountryID($countryID, $numCountries)
 {
-	//As countries are numbered starting at one, the count of the array is equal to the highest value country.
-	//Check > 0 and <= the number of countries as a country id can't be less than 1, or more than the number of
-	//countries
+    //As countries are numbered starting at one, the count of the array is equal to the highest value country.
+    //Check > 0 and <= the number of countries as a country id can't be less than 1, or more than the number of
+    //countries
     return($countryID < $numCountries && $countryID >= 0);
 }
  
@@ -209,26 +237,26 @@ function IsValidCountryID($countryID, $numCountries)
 // POST: FCTVAL == array of the names of all statistics in the database
 function GetStatNames($database)
 {
-	//returning array
-	$statArray = array();
-	
-	$statQuery = "SELECT stat_name FROM meta_stats";
-	$statResult = $database->query($statQuery);
-	
-	//If the table is empty, statResults will have a value of false. If this happens something has deleted
-	//the meta_stats table - refer to the mySQL architecture
-	if($statResult === false)
-	{
-		ThrowFatalError("MySQL Architecture error");
-	}
-	
-	//Put each row's column of "stat_name" into an array
-	while($statRow = $statResult->fetch_assoc())
-	{
-		array_push($statArray, $statRow['stat_name']);
-	}
-	
-	return $statArray;
+    //returning array
+    $statArray = array();
+    
+    $statQuery = "SELECT stat_name FROM meta_stats";
+    $statResult = $database->query($statQuery);
+    
+    //If the table is empty, statResults will have a value of false. If this happens something has deleted
+    //the meta_stats table - refer to the mySQL architecture
+    if($statResult === false)
+    {
+        ThrowFatalError("MySQL Architecture error");
+    }
+    
+    //Put each row's column of "stat_name" into an array
+    while($statRow = $statResult->fetch_assoc())
+    {
+        array_push($statArray, $statRow['stat_name']);
+    }
+    
+    return $statArray;
 }
   
  
@@ -242,31 +270,31 @@ function GetStatNames($database)
 // POST: return an array with exactly two indices: index 0 is the lowest year, index 1 is the highest year  
 function GetYearRange($database, $table)
 {
-	$indexOfFirstYearColumn = 1;
-	$descriptionArray = array();
-	$yearRange = array();
-		
-	//Create the query and query the query
+    $indexOfFirstYearColumn = 1;
+    $descriptionArray = array();
+    $yearRange = array();
+        
+    //Create the query and query the query
     $descriptionQuery = "DESCRIBE " . $table;
-	$descriptionResult = $database->query($descriptionQuery);
-	
-	if($descriptionResult === false)
-	{
-		ThrowFatalError("Invalid table name for year range.");
-	}
-	
-	//Throw each column's 'field' value into an array 
-	while($descriptionRow = $descriptionResult->fetch_assoc())
-	{
-		array_push($descriptionArray, $descriptionRow['Field']);
-	} 
-	
-	//Put the first and last year into the returning array
-	array_push($yearRange, $descriptionArray[$indexOfFirstYearColumn]);
-	array_push($yearRange, $descriptionArray[count($descriptionArray) - 1]);
-	
-	
-	return $yearRange;
+    $descriptionResult = $database->query($descriptionQuery);
+    
+    if($descriptionResult === false)
+    {
+        ThrowFatalError("Invalid table name for year range.");
+    }
+    
+    //Throw each column's 'field' value into an array 
+    while($descriptionRow = $descriptionResult->fetch_assoc())
+    {
+        array_push($descriptionArray, $descriptionRow['Field']);
+    } 
+    
+    //Put the first and last year into the returning array
+    array_push($yearRange, $descriptionArray[$indexOfFirstYearColumn]);
+    array_push($yearRange, $descriptionArray[count($descriptionArray) - 1]);
+    
+    
+    return $yearRange;
 } //END GetYearRange
 
 // Returns an array of queries given a set of tables to be queried, and the countries to be queried
