be17844744c67dad1119fdeec22a0f62e1642ff1
jcrafts318 <jcrafts318@gmail.com>
2015-04-17 04:42:41 -0400

    Added Copyright Notice to All Files

--- src/API/api_library.php
+++ src/API/api_library.php
@@ -1,4 +1,25 @@
 <?php
+/*
+ * Copyright 2015 Stateware Team: William Bittner, Joshua Crafts, 
+ * Nicholas Denaro, Dylan Fetch, Paul Jang, Arun Kumar, Drew Lopreiato,
+ * Kyle Nicholson, Emma Roudabush, Berty Ruan, Vanajam Soni
+ * 
+ * This file is part of Dav3i.
+ * 
+ * Dav3i is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Dav3i is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Dav3i.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /* File Name:           api_library.php
  * Description:         This file holds all of the functions which the api calls will return as well as helper functions to those calls
  * Date Created:        2/19/2015
--- src/GUI/settings.js
+++ src/GUI/settings.js
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2015 Stateware Team: William Bittner, Joshua Crafts, 
+ * Nicholas Denaro, Dylan Fetch, Paul Jang, Arun Kumar, Drew Lopreiato,
+ * Kyle Nicholson, Emma Roudabush, Berty Ruan, Vanajam Soni
+ * 
+ * This file is part of Dav3i.
+ * 
+ * Dav3i is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Dav3i is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Dav3i.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // File Name:               settings.js
 // Description:             Module to modify and work with settings
 // Date Created:            3/26/2015
