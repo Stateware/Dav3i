2f6abe10bf1718b981f8230e1cbbb2098d87c304
jcrafts318 <jcrafts318@gmail.com>
2015-08-24 00:00:54 +0000

    automatic build update [ci skip]

--- prod/index_lib.js
+++ prod/index_lib.js
@@ -1,4 +1,4 @@
-/*! Dav3i - v0.1.0 - 2015-08-22
+/*! Dav3i - v0.1.0 - 2015-08-24
 * https://github.com/Stateware/Dav3i
 * Copyright (c) 2015 Stateware Team;
  Licensed GPL v3 (https://github.com/Stateware/Dav3i/blob/master/LICENSE) */
@@ -119,7 +119,7 @@ $(function(){
                 g_HMSReady = true;
             }
         });
-    }
+    };
 
     map = new jvm.Map(
     {
---
+++ prod/upload_lib.js
@@ -0,0 +1,12 @@
+/*! Dav3i - v0.1.0 - 2015-08-24
+* https://github.com/Stateware/Dav3i
+* Copyright (c) 2015 Stateware Team;
+ Licensed GPL v3 (https://github.com/Stateware/Dav3i/blob/master/LICENSE) */
+function SelectType()
+{
+	document.getElementById('lin').style.display='none';
+	document.getElementById('est').style.display='none';
+	document.getElementById('bar').style.display='none';
+	document.getElementById('int').style.display='none';
+	document.getElementById(document.getElementById('graph-type').value).style.display='block';
+}
