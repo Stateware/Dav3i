cdf3a48058d5df3c8b490d5e3785b0223de6185f
jcrafts318 <jcrafts318@gmail.com>
2015-09-03 21:54:40 +0000

    automatic build update [ci skip]

--- prod/app_lib.min.js
+++ prod/app_lib.min.js
@@ -1,4 +1,4 @@
-/*! Dav3i - v0.1.0 - 2015-09-01
+/*! Dav3i - v0.1.0 - 2015-09-03
 * https://github.com/Stateware/Dav3i
 * Copyright (c) 2015 Stateware Team;
  Licensed GPL v3 (https://github.com/Stateware/Dav3i/blob/master/LICENSE) */
