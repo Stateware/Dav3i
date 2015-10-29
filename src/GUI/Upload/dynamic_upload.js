5855ede0ff3cf6c0cb5eb42fd9888122cc4e4923
Paul Jang <pjang94@gmail.com>
2015-10-29 19:09:11 -0400

    data upload page now creates appropriate session divs when the add session button is pressed

--- src/GUI/Upload/dynamic_upload.js
+++ src/GUI/Upload/dynamic_upload.js
@@ -1,25 +1,60 @@
-var scenario_counter = 1;
-function addScenario(divName){
-    var newdiv = document.createElement('div');
-    newdiv.innerHTML = "<input type='text' placeholder='Scenario Name' name='myInputs[]'>";
-    document.getElementById(divName).appendChild(newdiv);
-    counter++;
-    var btn = document.createElement("button");
-    var text = document.createTextNode("Edit Scenario");
-    btn.appendChild(text);
-    btn.style.textAlign="center";
-    document.getElementById(divName).appendChild(btn);
+// creates a div with the name divname and adds it to the parent div specified
+function addDiv(divName, parent)
+{
+    // create new div and assign it given id
+    var newDiv = document.createElement('div');
+    newDiv.id = divName;
+
+    // append it to body of page, and then to the parent div
+    document.getElementsByTagName('body')[0].appendChild(newDiv);
+    document.getElementById(parent).appendChild(newDiv);
+
+    // return divName, or null if element was not created correctly
+    return newDiv;
 }
 
-var session_counter = 1;
-function addSession(divName){
-    var newdiv = document.createElement('div');
-    newdiv.innerHTML = "<input type='text' placeholder='Session Name' name='myInputs[]'>";
-    document.getElementById(divName).appendChild(newdiv);
-    counter++;
-    var btn = document.createElement("button");
-    var text = document.createTextNode("Edit Session");
-    btn.appendChild(text);
-    btn.style.textAlign="center";
-    document.body.appendChild(btn);
+// adds a name field to the parent div given, with the placeholder text given
+function addNameField(placeholderText,parentDivName)
+{
+    // creating the session name field div
+    var nameFieldDiv = document.createElement('div');
+
+    // creating the id that the name field with have
+    var nameFieldId = parentDivName + "-name-field";
+    nameFieldDiv.id = nameFieldId;
+
+    // creating the input text form
+    nameFieldDiv.innerHTML = "<input type='text' placeholder="+ placeholderText +">";
+
+    // append it to the session div
+    document.getElementsByTagName('body')[0].appendChild(nameFieldDiv);
+    document.getElementById(parentDivName).appendChild(nameFieldDiv);
+    
+    // returns null if element was not created correctly
+    return document.getElementById(nameFieldId);
+}
+
+// adds a session div with the given divName, to the parent div given
+function addSession(divName,parent)
+{
+    // creates the new div
+    var sessionDiv = addDiv(divName,parent);
+    var placeholderText = "Session Name";
+
+    // add the name field to the session div
+    addNameField(placeholderText,sessionDiv.id);
+    addButton("Add Instance", sessionDiv.id);
+}
+
+// adds a button with the given text to the div provided
+function addButton(text,parentDivName)
+{
+    // creates the button and sets the values accordingly
+    var button = document.createElement('input');
+    button.type = 'button';
+    button.value = text;
+
+    // appends the div to the parent div given
+    var parentDiv = document.getElementById(parentDivName);
+    parentDiv.appendChild(button);
 }
