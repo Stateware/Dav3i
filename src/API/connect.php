1b34a8685d067d8e0d1a97aba4cb8f8d3d0c554e
Paul Jang <pjang94@gmail.com>
2015-12-03 18:45:30 -0500

    updated all the file headers to include new Dav3i contributors this semester as well as change the license from GPL to MIT.

--- src/API/connect.php
+++ src/API/connect.php
@@ -2,22 +2,21 @@
 /*
  * Copyright 2015 Stateware Team: William Bittner, Joshua Crafts, 
  * Nicholas Denaro, Dylan Fetch, Paul Jang, Arun Kumar, Drew Lopreiato,
- * Kyle Nicholson, Emma Roudabush, Berty Ruan, Vanajam Soni
+ * Brent Mosier, Kyle Nicholson, Emma Roudabush, Berty Ruan, Vanajam Soni, 
+ * Murlin Wei, Zekun Yang
  * 
  * This file is part of Dav3i.
  * 
  * Dav3i is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the MIT Software License.
  * 
  * Dav3i is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MIT Software License for more details.
  * 
- * You should have received a copy of the GNU General Public License
- * along with Dav3i.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the MIT Software License along 
+ * with Dav3i.  If not, see <https://opensource.org/licenses/MIT/>.
  */
 
 /* File Name:           connect.php
