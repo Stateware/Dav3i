8be451bf3c0ecd823c3a6618811737db07135b61
Nicholas Denaro <NicholasCDenaro@aol.com>
2016-02-01 19:33:25 -0500

    Add documentation for ModifyData and GetData and t_AsdsNode.

--- src/GUI/data_query.js
+++ src/GUI/data_query.js
@@ -47,12 +47,32 @@ function ParseData(json)
 }
 
 
-// Author:          Vanajam Soni, Paul Jang
-// Date Created:    3/5/15
-// Last Modified:   3/26/15 by Vanajam Soni
-// Description:     Makes Ajax call to get country data from server
-// PRE: cid is a valid country-id
-// POST: FCTVAL == Ajax object that makes the API call with the given cid
+/*
+ *  Function: GetData
+ *  
+ *  Makes Ajax call to get country data from server
+ *
+ *  Parameters:
+ *      cid - The country to get the data from
+ *
+ *  Pre:
+ *      cid is a valid country-id
+ *
+ *  Post:
+ *      FCTVAL == Ajax object that makes the API call with the given cid
+ *
+ *  Returns:
+ *      Ajax object that makes the API call with the given cid
+ *
+ *  Authors:
+ *      Vanajam Soni, Paul Jang
+ *
+ *  Date Created:
+ *      3/5/15
+ *
+ *  Last Modified:
+ *      3/26/15 by Vanajam Soni
+ */
 function GetData(cid)
 {
     return $.ajax({                                      
@@ -66,13 +86,32 @@ function GetData(cid)
 }
 
 
-// Author: Vanajam Soni, Kyle Nicholson
-// Date Created: 3/24/15
-// Last Modified: 10/8/15 Nicholas Denaro
-// Description: adds or removes a node to the g_DataList to reflect the chosen regions on the map
-// PRE: selectedRegions is a string array of regions selected on the map
-// POST: modifies g_DataList if there is a mismatch between regions selected, and regions stored in the g_DataList. 
-// FCTVAL == linked list of currently selected countries on map.
+/*
+ *  Function: ModifyData
+ *  
+ *  Adds or removes a node to the g_DataList to reflect the chosen regions on the map
+ *
+ *  Parameters:
+ *      selectedRegions - The regions selected from the JVectorMap
+ *
+ *  Pre:
+ *      selectedRegions is a string array of regions selected on the map
+ *
+ *  Post:
+ *      modifies g_DataList if there is a mismatch between regions selected, and regions stored in the g_DataList.
+ *
+ *  Returns:
+ *      linked list of currently selected countries on map.
+ *
+ *  Authors:
+ *      Vanajam Soni, Kyle Nicholson
+ *
+ *  Date Created:
+ *      3/24/15
+ *
+ *  Last Modified:
+ *      10/8/15 Nicholas Denaro
+ */
 function ModifyData(selectedRegions) 
 {
     if(g_DataList == null)
